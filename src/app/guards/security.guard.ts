ebbfa76df07a35005e51402fb71e8df5bc6ccb51
Mohan Ram <mohanramphp@gmail.com>
2024-01-27 14:23:03 +0530

    15 to 16 migration

--- src/app/guards/security.guard.ts
+++ src/app/guards/security.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SecurityGuard implements CanActivate {
+export class SecurityGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
